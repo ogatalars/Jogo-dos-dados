838f416836dee98c8aed2ba523bc3021bc2330f5
F.Ogata <82166626+wolflars@users.noreply.github.com>
2021-07-31 12:59:29 -0300

    second commit
    Mudanças feitas pelo Aron Koffler.

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
-let randomNumber1 = Math.floor(Math.random()*6 +1);
-console.log(randomNumber1)
-
-let randomDice = "dice" + randomNumber1 + ".png";
-
-let randomImageSource = "images/" + randomDice 
-
-
-let image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
-
-let randomNumber2 = Math.floor(Math.random()*6 + 1);
-
-let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-let image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-if(randomNumber1 > randomNumber2) { 
-    document.querySelector("h1").innerHTML = "Jogador 1 ganhou!";
-}
-else if (randomNumber1 < randomNumber2) { 
-    document.querySelector("h1").innerHTML = "Jogador 2 ganhou!"
-}
-else { 
-    document.querySelector("h1").innerHTML = "Empate"
-}
-// if I click on the refresh page then a random number is going to start and them the dice is going to change
+const game = {
+    firstPlayer: {
+      id: 0,
+      number: null,
+    },
+    secondPlayer: {
+      id: 1,
+      number: null,
+    },
+    rollDices: function () {
+      this.firstPlayer.number = generateRandomDiceNumber();
+      this.secondPlayer.number = generateRandomDiceNumber();
+    },
+    renderDices: function () {
+      const imgElements = document.querySelectorAll("img");
+      imgElements[this.firstPlayer.id].setAttribute(
+        "src",
+        getDiceImage(this.firstPlayer.number)
+      );
+      imgElements[this.secondPlayer.id].setAttribute(
+        "src",
+        getDiceImage(this.secondPlayer.number)
+      );
+    },
+    renderWinner: function () {
+      const { number: p1num } = this.firstPlayer;
+      const { number: p2num } = this.secondPlayer;
+      const headerElement = document.querySelector("h1");
+      if (p1num > p2num) {
+        headerElement.innerHTML = "Jogador 1 ganhou!";
+        return;
+      }
+      if (p1num < p2num) {
+        headerElement.innerHTML = "Jogador 2 ganhou!";
+        return;
+      }
+      headerElement.innerHTML = "Empate";
+    },
+  };
+  function generateRandomDiceNumber() {
+    const min = Math.ceil(1);
+    const max = Math.floor(6);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  function getDiceImage(number) {
+    return `images/dice${number.toString()}.png`;
+  }
+  game.rollDices();
+  game.renderDices();
+  game.renderWinner();
